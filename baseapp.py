c06d8b8f271285c944a174aec25829c4e5f509a9
SimonBelths <beletskiysemen@gmail.com>
2025-02-13 13:01:43 +0500

    Версия 4.4
    Добавлен базовый класс для Tkinter-приложений с настройкой биндингов для клавиш
    
    - Реализован класс `BaseTkApp`, который автоматически настраивает биндинги для популярных комбинаций клавиш.
    - Комбинации клавиш включают: Ctrl+Z, Ctrl+Y, Ctrl+S, Ctrl+O, Ctrl+P, Ctrl+F, Ctrl+H, Ctrl+N, Ctrl+W, Ctrl+Q и дополнительные (Ctrl+T, Ctrl+Shift+Z, Ctrl+Enter, Ctrl+D, Ctrl+Shift+V).
    - Упростил процесс работы с горячими клавишами при разработке Tkinter-приложений, устранив проблемы с русской раскладкой.

---
+++ baseapp.py
@@ -0,0 +1,77 @@
+﻿import tkinter as tk
+
+
+class BaseTkApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.bind_hotkeys()
+
+    def bind_hotkeys(self):
+        # Основные комбинации клавиш
+        self.bind("<Control-KeyPress-z>", self._ctrl_z)
+        self.bind("<Control-KeyPress-y>", self._ctrl_y)
+        self.bind("<Control-KeyPress-s>", self._ctrl_s)
+        self.bind("<Control-KeyPress-o>", self._ctrl_o)
+        self.bind("<Control-KeyPress-p>", self._ctrl_p)
+        self.bind("<Control-KeyPress-f>", self._ctrl_f)
+        self.bind("<Control-KeyPress-h>", self._ctrl_h)
+        self.bind("<Control-KeyPress-n>", self._ctrl_n)
+        self.bind("<Control-KeyPress-w>", self._ctrl_w)
+        self.bind("<Control-KeyPress-q>", self._ctrl_q)
+
+        # Опциональные (если используешь в своих проектах)
+        self.bind("<Control-KeyPress-t>", self._ctrl_t)
+        self.bind("<Control-Shift-KeyPress-z>", self._ctrl_shift_z)
+        self.bind("<Control-KeyPress-Return>", self._ctrl_enter)
+        self.bind("<Control-KeyPress-d>", self._ctrl_d)
+        self.bind("<Control-Shift-KeyPress-v>", self._ctrl_shift_v)
+
+    def _ctrl_z(self, event):
+        print("Отмена действия (Ctrl+Z)")
+
+    def _ctrl_y(self, event):
+        print("Повтор действия (Ctrl+Y)")
+
+    def _ctrl_s(self, event):
+        print("Сохранение файла (Ctrl+S)")
+
+    def _ctrl_o(self, event):
+        print("Открытие файла (Ctrl+O)")
+
+    def _ctrl_p(self, event):
+        print("Печать (Ctrl+P)")
+
+    def _ctrl_f(self, event):
+        print("Поиск (Ctrl+F)")
+
+    def _ctrl_h(self, event):
+        print("Замена (Ctrl+H)")
+
+    def _ctrl_n(self, event):
+        print("Создание нового документа (Ctrl+N)")
+
+    def _ctrl_w(self, event):
+        print("Закрытие окна (Ctrl+W)")
+
+    def _ctrl_q(self, event):
+        print("Выход из приложения (Ctrl+Q)")
+
+    def _ctrl_t(self, event):
+        print("Создание новой вкладки (Ctrl+T)")
+
+    def _ctrl_shift_z(self, event):
+        print("Повтор отменённого действия (Ctrl+Shift+Z)")
+
+    def _ctrl_enter(self, event):
+        print("Вставка разрыва строки (Ctrl+Enter)")
+
+    def _ctrl_d(self, event):
+        print("Дублирование строки (Ctrl+D)")
+
+    def _ctrl_shift_v(self, event):
+        print("Вставка без форматирования (Ctrl+Shift+V)")
+
+
+if __name__ == "__main__":
+    app = BaseTkApp()
+    app.mainloop()
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from gui import create_gui
+from baseapp import BaseApp
+
+class MyApp(BaseApp):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Теперь можешь добавить собственные виджеты
+        self.title("Моё приложение")
+        text = tk.Text(self, width=50, height=10)
+        text.pack(padx=10, pady=10)
+        text.insert("end", "Привет, братик!")
 
 if __name__ == "__main__":
-    create_gui()
+    app = MyApp()
+    app.mainloop()
